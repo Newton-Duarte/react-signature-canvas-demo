5107a3dc862ce99ef8a9c6d5f48acd9bca81e9b0
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-05-30 19:47:17 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useRef, useState } from "react";
+import SignatureCanvas from "react-signature-canvas";
+import "./styles.css";
+
+export default function App() {
+  const [imageURL, setImageURL] = useState();
+  const sigCanvas = useRef();
+
+  const create = () => {
+    const URL = sigCanvas.current.getTrimmedCanvas().toDataURL("image/png");
+    setImageURL(URL);
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <SignatureCanvas
+        ref={sigCanvas}
+        penColor="black"
+        canvasProps={{ className: "sigCanvas" }}
+      />
+      <button onClick={create}>Save</button>
+      <button onClick={() => sigCanvas.current.clear()}>Clear</button>
+
+      {imageURL && (
+        <>
+          <h1>Image saved</h1>
+          <img src={imageURL} alt="signature" className="signature" />
+        </>
+      )}
+    </div>
+  );
+}
